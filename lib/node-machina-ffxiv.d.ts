be2769da86d615720580e3ad5dc9dd99a9a7049a
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-04-14 00:00:10 -0700

    Delete node-machina-ffxiv.d.ts

--- lib/node-machina-ffxiv.d.ts
+++
@@ -1,5 +0,0 @@
-import { ZanarkandFFXIVOptions } from "./models/ZanarkandFFXIVOptions";
-export declare class ZanarkandFFXIV {
-    private options;
-    constructor(options: ZanarkandFFXIVOptions);
-}
